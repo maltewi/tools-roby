667b60ef0810c050b4e74525e9bc4b6383ad28ae
Malte Wirkus <malte.wirkus@dfki.de>
2015-02-26 14:37:35 +0100

    updated 'sleep' method from task scripts to successfully finish after the given time

--- lib/roby/tasks/timeout.rb
+++ lib/roby/tasks/timeout.rb
@@ -5,11 +5,27 @@ module Roby
             terminates
 
             event :timed_out
-            forward :timed_out => :stop
+            forward :timed_out => :success
 
             event :start do |context|
                 forward_to :start, self, :timed_out, :delay => delay
                 emit :start
+            end
+
+            on :start do |event|
+                start_time = Time.now
+            end
+
+            poll do
+                puts "waiting..."
+                if (Time.now - start_time) >= delay
+                    puts "timeout hit"
+                    emit :timed_out
+                end
+            end
+
+            on :stop do |event|
+                puts "Timeout stopped"
             end
         end
     end
